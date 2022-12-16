4a52fae7f640db45ac3b2c54e552fa65509b0554
zhuxining <zhuxining@outlook.com>
2022-12-16 17:31:46 +0800

    add VaR and ES

---
+++ JupyterStudy/import_sort.py
@@ -0,0 +1,9 @@
+import matplotlib.dates as mdates
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import scipy.stats as st
+from pylab import mpl
+
+mpl.rcParams["font.family"] = ["Hei"]
+mpl.rcParams["axes.unicode_minus"] = False
